e7b3d9db9ff26a27f3f6a718dcd2509bbabbc330
Felipe Miranda <felip.simi@gmail.com>
2020-07-03 14:43:44 -0300

    Preparacao: setup do projeto no Spring Initializr

---
+++ src/test/java/com/techtrilha/trilhainicial/errorhandling/ErrorHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtrilha.trilhainicial.errorhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
