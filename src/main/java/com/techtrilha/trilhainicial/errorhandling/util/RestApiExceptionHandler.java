09dc2eef917a95ed98f27445f42351041424df27
Felipe Miranda <felip.simi@gmail.com>
2020-07-04 18:12:05 -0300

    Segundo passo: criacao da classe de config para tratamento de excecoes

---
+++ src/main/java/com/techtrilha/trilhainicial/errorhandling/util/RestApiExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.techtrilha.trilhainicial.errorhandling.util;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class RestApiExceptionHandler {
+}
