40c9e821906f8e7b6ad37a4bf4d704ad0d5bb47d
Felipe Miranda <felip.simi@gmail.com>
2020-07-04 18:50:00 -0300

    Quarto passo: inclusao do tratamento de excecoes da classe base

--- src/main/java/com/techtrilha/trilhainicial/errorhandling/util/RestApiExceptionHandler.java
+++ src/main/java/com/techtrilha/trilhainicial/errorhandling/util/RestApiExceptionHandler.java
@@ -1,7 +1,18 @@
 package com.techtrilha.trilhainicial.errorhandling.util;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class RestApiExceptionHandler {
+
+  @ExceptionHandler(value = {RestApiException.class})
+  protected ResponseEntity<ErrorResponse> handleApiException(RestApiException error) {
+    final var statusCode = error.getStatusCode();
+    final var responseBody = error.getErrorBody();
+    return ResponseEntity.status(statusCode)
+        .body(responseBody);
+  }
+
 }
